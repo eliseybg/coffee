19a1418b3c6e7b34fbc26b3cd86b0e325866dfbf
Elisey Budnitsky <bgs6544@gmail.com>
2022-03-14 10:50:38 +0300

    splash screen

---
+++ madcoffeego/app/src/main/java/com/breaktime/madcoffeego/SplashScreenFragment.kt
@@ -0,0 +1,16 @@
+package com.breaktime.madcoffeego
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SplashScreenFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+}
